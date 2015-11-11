8a598927aa8d39f361c0a848998a95c733c58ef9
Yaan1994 <strautz_bogdan@yahoo.com>
2015-11-11 23:40:57 +0200

    Am creat metodele de get si set in clasele Student si AdministratieCamin din proiectul Camin

--- Facultate/Camin/Program.cs
+++ Facultate/Camin/Program.cs
@@ -8,15 +8,77 @@ namespace Camin
 {
     class Student
     {
-        private String nume { get; set; }
-        private String prenume { get; set; }
-        private int nr_matricol { get; set; }
-        private float medie { get; set; }
+        private string nume;
+        private string prenume;
+        private int nr_matricol;
+        private float medie;
+
+        public Student(string nume, string prenume, int nr_matricol)
+        {
+            setNume(nume);
+            setPrenume(prenume);
+            setNr_matricol(nr_matricol);
+        }
+
+        public void setNume(string nume)
+        {
+            this.nume = nume;
+        }
+
+        public string getNume()
+        {
+            return nume;
+        }
+
+        public void setPrenume(string prenume)
+        {
+            this.prenume = prenume;
+        }
+
+        public string getPrenume()
+        {
+            return prenume;
+        }
+
+        public void setNr_matricol(int nr_matricol)
+        {
+            this.nr_matricol = nr_matricol;
+        }
+
+        public int getNr_matricol()
+        {
+            return nr_matricol;
+        }
+
+        public void setMedie(float medie)
+        {
+            this.medie = medie;
+        }
+
+        public float getMedie()
+        {
+            return medie;
+        }
     }
 
     class AdministratieCamin
     {
-        private String nume { get; set; }
+        private string nume;
         private List<Student> student;
+
+        public void setNume(string nume)
+        {
+            this.nume = nume;
+        }
+
+        public string getNume()
+        {
+            return nume;
+        }
+
+        public AdministratieCamin(string nume)
+        {
+            setNume(nume);
+        }
     }
 }
