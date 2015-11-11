865d1d5ae8c28028727ff9a87b328ab0db9b93c0
Yaan1994 <strautz_bogdan@yahoo.com>
2015-11-11 22:59:23 +0200

    Am creat proiectul 'Camin'

---
+++ Facultate/Camin/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Camin
+{
+    class Student
+    {
+        private String nume { get; set; }
+        private String prenume { get; set; }
+        private int nr_matricol { get; set; }
+        private float medie { get; set; }
+    }
+
+    class AdministratieCamin
+    {
+        private String nume { get; set; }
+        private List<Student> student;
+    }
+}
