992e7c01f8ed54e3fc529f98632d01bbe6636179
Yaan1994 <strautz_bogdan@yahoo.com>
2015-11-16 00:27:03 +0200

    Defineste enum-ul din prj 'Camin' intr-o clasa noua!

---
+++ Facultate/Baza_de_date/Tabel_studenti.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Tabel_studenti]
+(
+	[Nr_matricol] INT NOT NULL PRIMARY KEY, 
+    [Nume] NVARCHAR(50) NOT NULL, 
+    [Prenume] NVARCHAR(50) NOT NULL, 
+    [Note] TEXT NULL, 
+    [An] INT NOT NULL, 
+    [Facultate] VARCHAR(50) NOT NULL, 
+    [Cazare_camin] BIT NULL DEFAULT 0, 
+    [Bursa] BIT NULL DEFAULT 0
+)
--- Facultate/Camin/Student.cs
+++ Facultate/Camin/Student.cs
@@ -4,22 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace Camin
 {
+    //ACEST ENUM AR TREBUI DEFINIT CA O CLASA NOUA!
+    enum stare { Neinteresat = 0, Interesat, Cazat, Fara_loc };
+
     class Student
     {
         private string nume {get; internal set;}
         private string prenume { get; internal set; }
         private int nr_matricol { get; internal set; }
         private float medie { get; internal set; }
-        private bool cazare { get; internal set; }
+        private stare stare { get; internal set; } 
 
-        internal Student(string nume, string prenume, int nr_matricol, bool cazare)
+        internal Student(string nume, string prenume, int nr_matricol, stare stare)
         {
             this.nume = nume;
             this.prenume = prenume;
             this.nr_matricol = nr_matricol;
-            this.cazare = cazare;
+            this.stare = stare;
             //apeleaza secretariat pentru medie
         }
 
