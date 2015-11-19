8125dbf13aa9e6452773b2dfb2eea6005fce097f
Yaan1994 <strautz_bogdan@yahoo.com>
2015-11-19 21:08:26 +0200

    Library Classes for 'Camin_' and 'Facultate_'

---
+++ Facultate/Camin_/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Camin_
+{
+    //ACEST ENUM AR TREBUI DEFINIT CA O CLASA NOUA!
+    enum stare { Neinteresat = 0, Interesat, Cazat, Fara_loc };
+
+    class Student
+    {
+        private string nume { get; internal set; }
+        private string prenume { get; internal set; }
+        private int nr_matricol { get; internal set; }
+        private float medie { get; internal set; }
+        private stare stare { get; internal set; }
+
+        internal Student(string nume, string prenume, int nr_matricol, stare stare)
+        {
+            this.nume = nume;
+            this.prenume = prenume;
+            this.nr_matricol = nr_matricol;
+            this.stare = stare;
+            //apeleaza secretariat pentru medie
+        }
+
+        internal override bool Equals(object obj)
+        {
+            var student = (Student)obj;
+            if (student != null)
+            {
+                if (student.nr_matricol == this.nr_matricol)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
